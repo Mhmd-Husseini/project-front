c47ab9febd29601e81f851cf8634ce63436aab6e
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-16 23:46:28 +0300

    Client <react> redesign enrollment page

--- src/components/shared/teacher_person/index.jsx
+++ src/components/shared/teacher_person/index.jsx
@@ -10,14 +10,14 @@ const Person = ({info, handleOpenModal}) => {
     <div className='t-body'>
       <div className='t-person-item t-student'>
         <span>Student: {info.name}</span>
-        <a href="#" className='icons' onClick={() => handleOpenModal(info)}>
-          <span>Comment</span>
+        <a href="#" className='t-icons' onClick={() => handleOpenModal(info)}>
+          <span>Chat</span>
         </a>
       </div>
       <div className='t-person-item'>
         <span>Parent: {info.parent.name}</span>
         <a href="#" className='t-icons' onClick={() => handleOpenModal(info)}>
-          <span>Comment</span>
+          <span>Chat</span>
         </a>  
       </div>
 
